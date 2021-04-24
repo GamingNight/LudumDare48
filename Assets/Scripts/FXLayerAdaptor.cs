73e4e827b62699765f408fa1f0aee8caa97d1532
JulesPomme <julian.moreira.fr@gmail.com>
2021-04-24 23:52:35 +0200

    layer adaptors

---
+++ Assets/Scripts/FXLayerAdaptor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXLayerAdaptor : MonoBehaviour
+{
+
+    public GlobalGameData globalGameDate;
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        animator.SetInteger("Layer Number", globalGameDate.GetCurrentLayerIndex());
+    }
+}
