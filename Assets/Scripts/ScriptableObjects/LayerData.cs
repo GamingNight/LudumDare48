d402eb553ad891b64d4239c5284bd1c447bca3d2
JulesPomme <julian.moreira.fr@gmail.com>
2021-04-24 09:17:24 +0200

    commit de dossiers

---
+++ Assets/Scripts/ScriptableObjects/LayerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Layer")]
+public class LayerData : ScriptableObject
+{
+
+    public int layer;
+}
