adca1885e8e43e1096f5f18a9788278963548ebb
julienfaure <julien.y.faure@gmail.com>
2021-04-25 23:18:25 +0200

    Revert "init method spawner generator"
    This reverts commit 93a11ff68b3e4df359ccd114b0f6ecbdfa3bd1fe.

--- Assets/Scripts/SpawnerGenerator.cs
+++ Assets/Scripts/SpawnerGenerator.cs
@@ -9,21 +9,17 @@ public class SpawnerGenerator : MonoBehaviour
     public float InstantiateDelayInSecond = 0f;
     public float spawnerFrequency = 0.2f;
     public Vector3 spawnerShift = new Vector3(0f, 0f, 0f);
-    public int n = 1;
+    public int N = 1;
     public LayerData layerData;
     public GlobalGameData globalGameData;
 
     private float timeSinceStart;
-    private bool instantiated;
+    private bool Instantiated;
     private Vector3 firstSpawnerAtOrigin = new Vector3(0.318f, 0.318f, 0);
     // Start is called before the first frame update
     void Start() {
-        Init();
-    }
-
-    public void Init() {
         timeSinceStart = 0;
-        instantiated = false;
+        Instantiated = false;
     }
 
     // Update is called once per frame
@@ -32,15 +28,15 @@ public class SpawnerGenerator : MonoBehaviour
         int projectileLayerIndex = layerData.layerIndex;
         float speedFactor = globalGameData.allLayers[currentLayerIndex].layerSpeedFactors[projectileLayerIndex];
         timeSinceStart += (Time.deltaTime * speedFactor);
-        if (timeSinceStart < InstantiateDelayInSecond || instantiated == true)
+        if (timeSinceStart < InstantiateDelayInSecond || Instantiated == true)
             return;
         else {
             InstantiateSpawner();
-            instantiated = true;
+            Instantiated = true;
         }
     }
     private void InstantiateSpawner() {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             GameObject spawner = Instantiate(projectileSpawnerPrefab, transform.position, transform.rotation, transform);
             spawner.transform.localPosition = new Vector3(spawner.transform.localPosition.x, spawner.transform.localPosition.y, spawner.transform.localPosition.z) + i * spawnerShift + firstSpawnerAtOrigin;
             ProjectileSpawner projectileSpawner = spawner.GetComponent<ProjectileSpawner>();
