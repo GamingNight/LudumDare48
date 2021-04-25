f2919660f2ba5824a6e3db5e4564f17e438840c0
vilboot <vilboot@hotmail.com>
2021-04-25 15:30:40 +0200

    go to the next level

--- Assets/Scripts/FinisherController.cs
+++ Assets/Scripts/FinisherController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FinisherController : MonoBehaviour
 {
+    private LevelManager levelManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +26,23 @@ public class FinisherController : MonoBehaviour
         	
         	if (index == 0)
         	{
-        		Debug.Log("You Win !! GG boy");
+                Transform t = transform;
+                int count = 0;
+                while (levelManager == null) {
+                    t = t.parent;
+                    levelManager = t.GetComponent<LevelManager>();
+                    count = count + 1;
+                    if (count > 5)
+                    {
+                        Debug.Log("infinit loop (deeper and deeper)");
+                        Debug.Log(count);
+                        return;
+                    }
+                    
+                }
+
+                levelManager.ActiveNextLevel();
+
         	}
             
         }
