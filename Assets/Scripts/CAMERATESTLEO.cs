0532ccdc3c6ed2dfccff5e5bd8354b1938611fdf
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2021-04-26 21:05:07 +0200

    lol

---
+++ Assets/Scripts/CAMERATESTLEO.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAMERATESTLEO : MonoBehaviour
+{
+    public GlobalGameData globalGameData;
+    public Animator anim;
+
+    private int lastIndex = -1;
+
+    private void Awake()
+    {
+        globalGameData.InitLayer();
+    }
+
+    void Start()
+    {
+        lastIndex = -1;
+    }
+
+    void Update()
+    {
+
+        int currentIndex = globalGameData.GetCurrentLayerIndex();
+        if (lastIndex != currentIndex)
+        {
+            lastIndex = currentIndex;
+            switch (lastIndex)
+            {
+                case 0:
+                    anim.SetInteger("Layer", 0);
+                    break;
+                case 1:
+                    anim.SetInteger("Layer", 1);
+                    break;
+                case 2:
+                    anim.SetInteger("Layer", 2);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
