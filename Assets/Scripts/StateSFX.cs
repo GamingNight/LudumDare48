1dd7b3b29ee6f622d6e7aee0adeed2817900c1a6
JulesPomme <julian.moreira.fr@gmail.com>
2021-04-26 12:10:00 +0200

    sfx sound

--- Assets/Scripts/StateSFX.cs
+++ Assets/Scripts/StateSFX.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class StateSFX : MonoBehaviour
 {
+    public GlobalGameData globalGameData;
 
-    public AudioSource transitionSFX;
+    private AudioSource transitionSFX;
+    private int previousLayerIndex;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            if (!transitionSFX.isPlaying)
-            {
-                transitionSFX.timeSamples = GetComponent<AudioSource>().clip.samples;
-                transitionSFX.pitch = 1;
-                transitionSFX.Play();
-            }
+    void Start() {
+
+        transitionSFX = GetComponent<AudioSource>();
+        previousLayerIndex = 0;
+    }
+
+    void Update() {
+
+        int currentLayerIndex = globalGameData.GetCurrentLayerIndex();
+        if (currentLayerIndex > previousLayerIndex) { //deeper
+            transitionSFX.time = 0;
             transitionSFX.pitch = 1;
+            transitionSFX.Play();
+        } else if (currentLayerIndex < previousLayerIndex) {  //shallower
+            transitionSFX.time = 0;
+            transitionSFX.pitch = 0.1f;
+            transitionSFX.Play();
         }
+        previousLayerIndex = currentLayerIndex;
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    if (!transitionSFX.isPlaying)
+        //    {
+        //        transitionSFX.timeSamples = GetComponent<AudioSource>().clip.samples;
+        //        transitionSFX.pitch = 1;
+        //        transitionSFX.Play();
+        //    }
+        //    transitionSFX.pitch = 1;
+        //}
 
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            if (!transitionSFX.isPlaying)
-            {
-                transitionSFX.timeSamples = GetComponent<AudioSource>().clip.samples - 1;
-                transitionSFX.pitch = -1.3f;
-                transitionSFX.Play();
-            }
-            transitionSFX.pitch = -1.3f;
-        }
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    if (!transitionSFX.isPlaying)
+        //    {
+        //        transitionSFX.timeSamples = GetComponent<AudioSource>().clip.samples - 1;
+        //        transitionSFX.pitch = -1.3f;
+        //        transitionSFX.Play();
+        //    }
+        //    transitionSFX.pitch = -1.3f;
+        //}
     }
 
 }
